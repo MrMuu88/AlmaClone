d7a8aa501cfb8d60b5e1b8a5f228c05dd598ad1c
Tamás József Simon <simont@infomix.hu>
2024-09-10 14:42:55 +0200

    header done

--- src/Components/TabHeaderComponent.tsx
+++ src/Components/TabHeaderComponent.tsx
@@ -1,14 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, StyleSheet, Alert } from 'react-native'
 import React from 'react'
 import { BottomTabHeaderProps } from '@react-navigation/bottom-tabs'
-import { colors } from '../Styles/StyleSheet'
 
 
 
 export default function TabHeaderComponent(props : BottomTabHeaderProps) {
   return (
-    <View>
-      <Text style={{color:colors.primary}}>TabHeaderComponent</Text>
-    </View>
+    <View style={styles.container}>
+        <TouchableOpacity style={styles.logoContainer} onPress={()=> Alert.alert("navigationg to Messages")}>
+          <Image source={require('../assests/icons/009-bell.png')} style={styles.logo}/>
+        </TouchableOpacity>
+
+        <View style={styles.logoContainer}>
+        <Image source={require('../assests/icons/Alma+logo_small.png')} style={styles.logo}/>
+        </View>
+
+        
+        <TouchableOpacity style={styles.logoContainer} onPress={()=> Alert.alert("navigationg to Messages")}>
+          <Image source={require('../assests/icons/019-menu.png')} style={styles.logo}/>
+        </TouchableOpacity>
+      </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container:{flexDirection:"row", justifyContent:"space-between", width:"100%", margin:8, padding:8},
+  logoContainer:{margin:16},
+  logo:{width:32, height:32, resizeMode:"contain"}
+});
--- src/Screens/Main/HomeScreen.tsx
+++ src/Screens/Main/HomeScreen.tsx
@@ -2,6 +2,7 @@ import { Text, View } from 'react-native'
 import React, { Component } from 'react'
 import { colors } from '../../Styles/StyleSheet'
 
+
 export default class HomeScreen extends Component {
   render() {
     return (
