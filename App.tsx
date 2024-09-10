d11d90a00319a7857796cd28a0cefab7f94e7307
Tamás József Simon <simont@infomix.hu>
2024-09-10 16:10:00 +0200

    stylized, navigation buttons

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, ReactNode } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from './src/Screens/Main/HomeScreen';
 import PharmanciesScreen from './src/Screens/Main/PharmanciesScreen';
@@ -8,19 +8,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import MainHeader from './src/Components/Headers/MainHeader';
 import { createStackNavigator } from '@react-navigation/stack';
 import MessagesScreen from './src/Screens/Other/MessagesScreen';
-import ProfileScreen from './src/Screens/Menu/ProfileScreen';
 import { MenuScreen } from './src/Screens/Menu/MenuScreen';
 import { MessagesHeader } from './src/Components/Headers/MessagesHeader';
 import { MenuHeader } from './src/Components/Headers/MenuHeader';
+import { Image, StyleSheet } from 'react-native';
+
+type NodeDictionary = {
+    [key: string]: ReactNode;
+  };
 
 const Tabs = ()=>{
+    const Icons :NodeDictionary ={
+        "Home":<Image style={styles.logo} source={require('./src/assests/icons/004-home.png')}/>,
+        "Coupon":<Image style={styles.logo} source={require('./src/assests/icons/005-coupon.png')}/>,
+        "Sales":<Image style={styles.logo} source={require('./src/assests/icons/030-pluspill.png')}/>,
+        "Pharmacies":<Image style={styles.logo} source={require('./src/assests/icons/008-location.png')}/>,
+
+    }
+
     const Tab = createBottomTabNavigator();
     return (
-        <Tab.Navigator screenOptions={{headerShown:false}}>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Coupon" component={CouponScreen} />
-            <Tab.Screen name="Sales" component={SalesScreen} />
-            <Tab.Screen name="Pharmacies" component={PharmanciesScreen} />
+        <Tab.Navigator screenOptions={{headerShown:false, tabBarStyle:{height:72,paddingBottom:16}}}>
+            <Tab.Screen options={{tabBarIcon:()=>{ return Icons["Home"]}}} name="Home" component={HomeScreen} />
+            <Tab.Screen options={{tabBarIcon:()=>{ return Icons["Coupon"]}}} name="Coupon" component={CouponScreen} />
+            <Tab.Screen options={{tabBarIcon:()=>{ return Icons["Sales"]}}} name="Sales" component={SalesScreen} />
+            <Tab.Screen options={{tabBarIcon:()=>{ return Icons["Pharmacies"]}}} name="Pharmacies" component={PharmanciesScreen} />
         </Tab.Navigator>
     );
 
@@ -40,3 +52,7 @@ export default function App() {
         </NavigationContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    logo:{width:24, height:24, resizeMode:"contain"}
+})
