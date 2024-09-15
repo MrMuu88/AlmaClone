7580546d98764819745b29fa3624d1d3e8a57470
Tamás József Simon <simont@infomix.hu>
2024-09-15 14:13:16 +0200

    finished Ptharmancy screen

--- src/Screens/Main/PharmanciesScreen.tsx
+++ src/Screens/Main/PharmanciesScreen.tsx
@@ -1,24 +1,28 @@
-import { Text, TextInput, View } from 'react-native'
+import { ScrollView, Text, TextInput, View } from 'react-native'
 import React, { Component } from 'react'
-import { colors } from '../../Styles/StyleSheet'
 import { Card, Icon } from '@rneui/themed'
 import { TouchableOpacity } from 'react-native'
+import PharmacyCard from '../../Components/HomeScreen/PharmacyCard'
 
 export default class PharmanciesScreen extends Component {
   render() {
     return (
-      <View>
-        <Card containerStyle={{ borderRadius: 4, marginBottom: 16 }} wrapperStyle={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", padding: 0 }}>
-          <TextInput style={{ flex: 1 }} placeholderTextColor={"gray"} placeholder="keresés termékekere, kategóriára..." />
-          <Icon name="search" size={32} />
-        </Card>
-        <View style={{ flexDirection: "row", alignItems:"center", justifyContent:"space-between", margin: 8}}>
-          <Text style = {{color:"black"}}>10 Termék</Text>
-          <TouchableOpacity style={{backgroundColor:"white",borderRadius:16, paddingHorizontal:8}}>
-            <Icon name="filter-list" />
-          </TouchableOpacity>
+      <View style={{flex:1}}>
+        <View>
+          <Card containerStyle={{ borderRadius: 4, marginBottom: 16 }} wrapperStyle={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", padding: 0 }}>
+            <TextInput style={{ flex: 1 }} placeholderTextColor={"gray"} placeholder="keresés termékekere, kategóriára..." />
+            <Icon name="search" size={32} />
+          </Card>
+          <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", margin: 16 }}>
+            <Text style={{ color: "black" }}>Elérhető Alma+ patikák</Text>
+            <TouchableOpacity style={{ backgroundColor: "white", borderRadius: 16, paddingHorizontal: 8 }}>
+              <Icon name="filter-list" />
+            </TouchableOpacity>
+          </View>
         </View>
-        <Text style={{ color: colors.primary }}>PharmanciesScreen</Text>
+        <ScrollView>
+          <PharmacyCard />
+        </ScrollView>
       </View>
     )
   }
